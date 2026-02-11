8a680b63496ea5fcac884a7b7134f0a61ffcbc1d
LeMill-Vycta <vyctalemill@gmail.com>
2026-02-11 22:23:21 +0300

    Remove vertical line and circles image from right accent

--- components/RightAccent.jsx
+++ components/RightAccent.jsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 const RightAccent = () => {
   return (
     <div
@@ -8,16 +6,6 @@ const RightAccent = () => {
     >
       <div className="absolute -right-20 top-[12%] h-64 w-64 rounded-full bg-accent/20 blur-[100px] md:h-80 md:w-80" />
       <div className="absolute right-[10%] top-[45%] h-56 w-56 rounded-full bg-cyan-400/15 blur-[95px] md:h-72 md:w-72" />
-      <div className="absolute inset-y-[8%] right-[20%] hidden w-px bg-gradient-to-b from-transparent via-white/20 to-transparent md:block" />
-      <Image
-        src="/circles.png"
-        alt=""
-        width={700}
-        height={700}
-        priority
-        sizes="(max-width: 1200px) 380px, 700px"
-        className="absolute -bottom-24 -right-32 hidden w-[420px] opacity-30 mix-blend-screen md:block xl:w-[560px]"
-      />
     </div>
   );
 };
